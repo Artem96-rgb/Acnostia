2f7ab56a62f0e9c8cc79992a802a38a435e0b368
@Pasko96-rgb <artempasko880@gmail.com>
2020-02-08 16:28:16 +0200

    add

--- js/flat.js
+++ js/flat.js
@@ -1,16 +1,25 @@
 
 // ----------------------------------slick slider----------------------------------------
-$(document).ready(function () {
-    $('.slick-header').slick({
-        dots: true
+// $(document).ready(function () {
+//     $('.slick-header').slick({
+//         dots: true
 
+//     });
+// });
+
+
+
+// var myFullpage = new fullpage('#fullpage', {
+//     autoScrolling: false,
+//     anchors: ['slick', 'serv', 'about'],
+//     menu: '#nav',
+// });
+
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
     });
-});
-
-
-
-var myFullpage = new fullpage('#fullpage', {
-    autoScrolling: false,
-    anchors: ['slick', 'serv', 'about'],
-    menu: '#nav',
-});
+}
